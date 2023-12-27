45b2d34e0e72e2bb6d06ee976ad45f70d49b5d5d
Muhammad Yazid Akbar <yazidakbar08@gmail.com>
2023-12-27 19:58:35 +0700

    euy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Home() {
-  return <div className="text-2xl">Profile Website</div>
+  return <div className="text-2xl">Portfolio Website</div>
 }
